8d5f6ae2df32ee762d3e04dd5c55e42aaae4f2a3
Samuel Enoque <thesonlike@gmail.com>
2023-04-06 18:57:43 -0300

    beforeNpmRunDev

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//initialize firebase app instance 
+//im having an problem with firebase import being declared but not read because
+//i have'nt initialized the app instance of firebase
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
